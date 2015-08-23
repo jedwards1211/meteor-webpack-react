50086f5bfd0419896bd2c327894374ac16549ba6
Andy Edwards <jedwards@fastmail.com>
2015-08-23 16:36:44 -0500

    used Meteor.startup instead of window.onload in main_client.js

--- app/main_client.js
+++ app/main_client.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import App from './components/App.jsx';
 
-window.webpackBundleLoaded = true;
-let prevOnload = window.onload;
-
-window.onload = function() {
-  if (prevOnload) prevOnload();
-  React.render(<App/>, document.getElementById('root'));
-};
-
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY',
 });
 
 console.log('Running on client only');
+
+Meteor.startup(() => {
+  React.render(<App/>, document.getElementById('root'));
+});

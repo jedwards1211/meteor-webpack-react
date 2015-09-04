4b0bd6bc2f6d6f9a43edaf2d483988f6ef3525d0
Adam Brodzinski <adambrodzinski@gmail.com>
2015-09-03 20:58:23 -0400

    uses a faster source map method

--- webpack/webpack.config.client.dev.js
+++ webpack/webpack.config.client.dev.js
@@ -5,13 +5,18 @@ var _ = require('lodash');
 var devProps = require('./devProps');
 
 var config = module.exports = _.assign(_.cloneDeep(config), {
-  devtool: 'source-map',
+  // http://webpack.github.io/docs/build-performance.html#sourcemaps
+  //devtool: 'eval',             // fastest - readable source maps
+  //devtool: 'source-map',       // slowest - best source maps
+  devtool: 'eval-source-map',    // best of both worlds
+  cache: true,
   entry: [
     'webpack-dev-server/client?' + devProps.baseUrl,
     'webpack/hot/only-dev-server',
   ].concat(config.entry),
   output: _.assign(_.cloneDeep(config.output), {
     publicPath: devProps.baseUrl + '/assets/',
+    pathinfo: true,
   }),
   plugins: (config.plugins || []).concat([
     new webpack.HotModuleReplacementPlugin(),
@@ -36,3 +41,4 @@ var jsLoader = _.find(config.module.loaders, function(loader) {
 if (jsLoader) {
   jsLoader.loader = 'react-hot!' + jsLoader.loader;
 }
+
